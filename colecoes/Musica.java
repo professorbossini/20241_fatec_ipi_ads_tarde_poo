0185305badf01f763d3190299296afd27cd604eb
Rodrigo Bossini <professorbossini@gmail.com>
2024-05-02 16:11:49 -0300

    feat(ap008): encerra a apostila
    - implementa menu com opcoes para ordenacoes por titulo e avaliacao
    - propoe exercicios envolvendo a interface comparator

--- colecoes/Musica.java
+++ colecoes/Musica.java
@@ -1,4 +1,4 @@
-public class Musica {
+public class Musica implements Comparable <Musica>{
   private String titulo;
   //1 a 5 (0 significa ainda não avaliada)
   private int avaliacao = 0;
@@ -11,6 +11,10 @@ public class Musica {
     return titulo;
   }
 
+  public int getAvaliacao() {
+    return avaliacao;
+  }
+
   public void avaliar(int avaliacao){
     this.avaliacao = avaliacao;
   }
@@ -21,5 +25,12 @@ public class Musica {
       "(titulo: %s, avaliacao: %d)",
       titulo, avaliacao
     );
+  }
+
+  public int compareTo(Musica m) {
+    //se o this for menor do que o m, devolver -1
+    //se forem iguais, devolver 0
+    //caso contrário, devolver 1
+    return this.getTitulo().compareTo(m.getTitulo());
   }
 }
